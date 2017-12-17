61956324523de5f89dc98ac19ce064ff767a64a7
kimonjo67 <monjokim@gmail.com>
2017-12-16 20:54:07 -0600

    First code push with trivia code

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+function check() {
+	// body...
+	
+// Create variables for the questions
+var question1 = document.quiz.exampleRadios.value;
+var question1 = document.quiz.exampleRadios.value;
+var question1 = document.quiz.exampleRadios.value;
+var question1 = document.quiz.exampleRadios.value;
+var correct = 0;
+
+	if (question1 === "Nairobi") {
+		correct++;
+	}
+document.getElementById("afterSubmit").style.visibility = "visible";
+document.getElementById("correctAnswer").innerHTML = "You got" + correct + "correct";
+
+}
